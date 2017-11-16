1ad6fb490ab497fdf18245b61a516f3ccf0fb57b
David Raznick <kindly@gmail.com>
2017-11-16 19:43:25 +0000

    Add options so that you can run the script in own notebook.

--- scripts/noteql.py
+++ scripts/noteql.py
@@ -1,13 +1,22 @@
 import sqlalchemy
 import ijson
 import os
-import json
+import simplejson as json
 import decimal
 import click
 import jinja2
+import functools
 from IPython.core.display import display, HTML
 
-engine = sqlalchemy.create_engine('postgres://noteql:noteql@db/noteql')
+
+
+
+@functools.lru_cache(128)
+def get_engine(dburi):
+    if not dburi:
+        dburi = 'postgres://noteql:noteql@db/noteql'
+    return sqlalchemy.create_engine(dburi)
+
 
 table = jinja2.Template(
 '''
@@ -39,14 +48,17 @@ def generate_rows(result, limit):
         yield [json.dumps(item, indent=2) if isinstance(item, dict) else item for item in row]
 
 class Session:
-    def __init__(self, schema, drop=False):
+
+    def __init__(self, schema, dburi=None, drop=False):
         self.schema = schema
-        with engine.begin() as connection:
+        self.engine = get_engine(dburi)
+        with self.engine.begin() as connection:
             if drop:
                 connection.execute('drop schema if extist {};'.format(self.schema))
             connection.execute('create schema if not exists {};'.format(self.schema))
+
     def run_sql(self, sql, limit=20):
-        with engine.begin() as connection:
+        with self.engine.begin() as connection:
             connection.execute('set search_path = {};'.format(self.schema))
             result = connection.execute(sql)
             if result.returns_rows:
@@ -57,6 +69,7 @@ class Session:
                 display(HTML(table.render(context)))
             else:
                 return "Success"
+
     def load_json(self, file_name, path_to_list='', table_name=None, field_name=None, append=False):
         load_json(file_name, path_to_list, self.schema + "." + table_name, field_name, append=False)
 
@@ -71,12 +84,14 @@ class DecimalEncoder(json.JSONEncoder):
 def put_quotes_round(table_name):
     return '.'.join('"{}"'.format(part) for part in table_name.split('.'))
 
-def load_json(file_name, path_to_list='', table_name=None, field_name=None, append=False):
+def load_json(file_name, path_to_list='', table_name=None, field_name=None, append=False, dburi=None):
     if not table_name:
         table_name = os.path.split(file_name)[1].split('.')[0]
     if not field_name:
         field_name = path_to_list.split('.')[-1] or 'json'
     table_name = put_quotes_round(table_name)
+
+    engine = get_engine(dburi)
 
     with engine.begin() as connection:
         if not append:
@@ -92,10 +107,12 @@ def load_json(file_name, path_to_list='', table_name=None, field_name=None, appe
 @click.option('--path', default='', help='path to list in json')
 @click.option('--table', default='', help='tablename')
 @click.option('--field', default='', help='fieldname in table')
+@click.option('--dburi', default='', help='sqlalchemy db uri')
 @click.option('--append', is_flag=True, help='prefix')
 @click.argument('filename')
-def load_json_command_line(filename, path, table, field, append):
-    load_json(filename, path, table, field, append)
+def load_json_command_line(filename, path, table, field, append, dburi):
+    load_json(filename, path, table, field, append, dburi)
+
 
 if __name__ == "__main__":
     load_json_command_line()
