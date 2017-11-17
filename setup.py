55f5613cdcd114f406ce26d6491d6490aa3ab92e
David Raznick <kindly@gmail.com>
2017-11-17 12:33:19 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='noteql',
+      version='0.1',
+      description='Write sql in a notbook',
+      author='David Raznick',
+      author_email='david.raznick@opendataservices.coop',
+      url='https://github.com/OpenDataServices/noteql',
+      packages=['noteql'],
+     )
+     
